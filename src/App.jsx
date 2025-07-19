d01b31a903844eeb2ede80d7fe5dbf0c561ed553
Yanaina06 <Yanainaferreira6@gmail.com>
2025-07-19 18:58:21 -0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import Malla from "./Malla";
+
+const root = document.getElementById("root");
+createRoot(root).render(<Malla />);
